453b268ddd2b8a9ad5aac15bc270c7a9c47d6d46
Hamza <hamzalahmou01@gmail.com>
2023-10-11 19:07:09 +0100

    Improved UI

--- Script.js
+++ Script.js
@@ -6,12 +6,14 @@
     let pSignImage = document.createElement('img');
     let computerScoreDisplay = document.querySelector('#computerScore');
     let playerScoreDisplay = document.querySelector('#playerScore');
+    let pScoreBox = document.querySelector('.pScoreBox');
+    let cScorebox = document.querySelector('.cScorebox');
     let rounds = 0;
     let computerScore = 0;
     let playerScore = 0;
     let result;
     let scoreMessage = document.querySelector('#scoreMessage');
-    function getComputerChoice() {
+    function getComputerChoice() {   
         let randomN = Math.floor(Math.random() * 3);
         let choice;
         switch (randomN) {
@@ -47,7 +49,7 @@
         }
         return playerSelection;
     }
-    function appendSign() {
+    function appendSign() { 
         playerSign.innerHTML = '';
         computerSign.innerHTML = '';
         playerSign.appendChild(pSignImage);
@@ -55,24 +57,38 @@
     }
     function score(playerSelection, Choice) {
         let display;
-        if (playerSelection === Choice) {
+        if (playerSelection === Choice) {            
+            pScoreBox.style.backgroundColor = '#56b856';
+            cScorebox.style.backgroundColor = '#56b856';
             display = "its a tie!";
-          } else if (playerSelection === R && Choice === P) {
+          } else if (playerSelection === R && Choice === P) {                        
+            pScoreBox.style.backgroundColor = '#e86c5d';
+            cScorebox.style.backgroundColor = '#6ac4c4';
             computerScore += 1;
             display = "you lost";
-          } else if (playerSelection === R && Choice === S) {
+          } else if (playerSelection === R && Choice === S) {            
+            pScoreBox.style.backgroundColor = '#6ac4c4';
+            cScorebox.style.backgroundColor = '#e86c5d';
             playerScore += 1;
             display = "you won!";
-          } else if (playerSelection === P && Choice === R) {
+          } else if (playerSelection === P && Choice === R) {            
+            pScoreBox.style.backgroundColor = '#6ac4c4';
+            cScorebox.style.backgroundColor = '#e86c5d';
             playerScore += 1;
             display = "you won!";
-          } else if (playerSelection === P && Choice === S) {
+          } else if (playerSelection === P && Choice === S) {            
+            pScoreBox.style.backgroundColor = '#e86c5d';
+            cScorebox.style.backgroundColor = '#6ac4c4';
             computerScore += 1;
             display = "you lost";
-          } else if (playerSelection === S && Choice === R) {
+          } else if (playerSelection === S && Choice === R) {            
+            pScoreBox.style.backgroundColor = '#e86c5d';
+            cScorebox.style.backgroundColor = '#6ac4c4';
             computerScore += 1;
             display = "you lost";
-          } else if (playerSelection === S && Choice === P) {
+          } else if (playerSelection === S && Choice === P) {            
+            pScoreBox.style.backgroundColor = '#6ac4c4';
+            cScorebox.style.backgroundColor = '#e86c5d';
             playerScore += 1;
             display = "you won!";
           } else {
@@ -98,6 +114,7 @@
                 scoreMessage.textContent = "Try again!";
                 playerSign.innerHTML = '';
                 computerSign.innerHTML = '';
+                alert("Play Again");
             }
         });
     });
